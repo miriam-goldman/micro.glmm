2233dc4a27d0998a5d9e8a647c10bee4468df6fd
Miriam Goldman <46382670+miriam-goldman@users.noreply.github.com>
2023-10-16 15:05:25 -0700

    Update parse_gene.py

--- python/parse_gene.py
+++ python/parse_gene.py
@@ -41,7 +41,7 @@ def read_genelen(glen_file,rep_genome):
     with open(glen_file) as instream:
         for line in instream:
             gene_id, genome_id, gene_len = line.rstrip().split('\t')
-            if genome_id not rep_genome:
+            if genome_id != rep_genome:
                 genome_dict[genome_id]=0
                 glen_dict[gene_id] = gene_len
             all_glen_dict[gene_id] = gene_len
