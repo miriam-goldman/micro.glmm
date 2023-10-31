7d95ab75058c1f251476cba0831909e504a665c7
Miriam Goldman <miriam.goldman@ucsf.edun>
2023-10-31 15:12:27 -0700

    fixed SPA

--- R/glmm_functions.R
+++ R/glmm_functions.R
@@ -825,7 +825,7 @@ Saddle_Prob<-function(q, mu, g, var1,Cutoff=2,output="P",log.p=FALSE)
   
   if(pval!=0 && pval.noadj/pval>10^3)
   {
-    return(Saddle_Prob(q, mu, g, Cutoff=Cutoff*2,output,log.p=log.p))
+    return(Saddle_Prob(q, mu, g,var1, Cutoff=Cutoff*2,output,log.p=log.p))
   } else if(output=="metaspline")
   {
     return(list(p.value=pval, p.value.NA=pval.noadj, Is.converge=Is.converge,Score=Score,splfun=splfun,var=var1))
